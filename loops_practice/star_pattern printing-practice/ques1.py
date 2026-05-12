07293d1626cc900afa7ddf40d8b41d140c880858
tripathi4251 <tripathi.4251@gmail.com>
2026-05-12 16:24:34 +0530

    add  pattern printing coding question

--- loops_practice/star_pattern printing-practice/ques1.py
+++ loops_practice/star_pattern printing-practice/ques1.py
@@ -2,3 +2,8 @@
 
 for i in range(1):
     print("****")
+
+# Print a Single Star (*) 
+
+for i in range(1):
+    print("*")
---
+++ loops_practice/star_pattern printing-practice/ques2.py
@@ -0,0 +1,6 @@
+# Print n Stars on Same Line
+
+n=int(input("enter the number of stars"))
+for i in range(n):
+     print("*",end="")
+     
